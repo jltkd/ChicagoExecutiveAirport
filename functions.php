54308e7c266976f94cb4741503fd621ee6ef006b
jltkd <jl@turnkeydigital.com>
2017-09-08 15:58:56 -0500

    Homepage updates

--- functions.php
+++ functions.php
@@ -23,3 +23,20 @@ add_filter( 'x_enqueue_parent_stylesheet', '__return_true' );
 // Additional Functions
 // =============================================================================
 
+function latestblogtitle($atts, $content=null){
+    $getposttitle = get_posts(array('number' => 1));
+    $getposttitle = $getposttitle[0];
+    $returntitle = $getposttitle->post_title;
+    return $returntitle;
+}
+add_shortcode('latestblogtitle', 'latestblogtitle');
+
+function latestblogdate($atts, $content=null){
+    $getpostdate = get_posts(array('number' => 1));
+    $getpostdate = $getpostdate[0];
+    $returndatemonth = mysql2date('m', $getpostdate->post_date);
+    $returndateyear = mysql2date('y', $getpostdate->post_date);
+    $returndatetotal = $returndatemonth . "." . $returndateyear;
+    return $returndatetotal;
+}
+add_shortcode('latestblogdate', 'latestblogdate');
